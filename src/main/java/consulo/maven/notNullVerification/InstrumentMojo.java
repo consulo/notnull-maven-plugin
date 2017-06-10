4051dfda85379d07aa8e9c12ef5428e861d98751
VISTALL <vistall.valeriy@gmail.com>
2017-06-10 17:39:09 +0300

    initial

---
+++ src/main/java/consulo/maven/notNullVerification/InstrumentMojo.java
@@ -0,0 +1,283 @@
+package consulo.maven.notNullVerification;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.lang.management.ManagementFactory;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.IOUtil;
+import org.jetbrains.org.objectweb.asm.ClassWriter;
+import com.intellij.compiler.instrumentation.FailSafeClassReader;
+import com.intellij.compiler.instrumentation.InstrumentationClassFinder;
+import com.intellij.compiler.instrumentation.InstrumenterClassWriter;
+import com.intellij.compiler.notNullVerification.NotNullVerifyingInstrumenter;
+import consulo.maven.notNullVerification.cache.CacheLogic;
+
+/**
+ * @author VISTALL
+ * @since 28-May-17
+ */
+@Mojo(name = "instrument", defaultPhase = LifecyclePhase.PROCESS_CLASSES, requiresOnline = false, requiresDependencyResolution = ResolutionScope.COMPILE)
+public class InstrumentMojo extends AbstractMojo
+{
+	@Parameter(property = "project", defaultValue = "${project}")
+	private MavenProject myMavenProject;
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException
+	{
+		try
+		{
+			String outputDirectory = myMavenProject.getBuild().getOutputDirectory();
+			File sourceDirectoryFile = new File(outputDirectory);
+			if(!sourceDirectoryFile.exists())
+			{
+				getLog().info(outputDirectory + " is not exists");
+				return;
+			}
+
+			List<File> files = FileUtils.getFiles(sourceDirectoryFile, "**/*.class", null);
+			if(files.isEmpty())
+			{
+				return;
+			}
+
+			InstrumentationClassFinder finder = buildFinder();
+
+			CacheLogic cacheLogic = new CacheLogic(myMavenProject, CacheLogic.NAME);
+
+			cacheLogic.read();
+
+			boolean changed = false;
+			for(File classFile : files)
+			{
+
+				if(cacheLogic.isUpToDate(classFile))
+				{
+					continue;
+				}
+
+				cacheLogic.removeCacheEntry(classFile);
+
+				byte[] data = null;
+				FileInputStream stream = null;
+				try
+				{
+					stream = new FileInputStream(classFile);
+					data = IOUtil.toByteArray(stream);
+				}
+				catch(Exception e)
+				{
+					getLog().error(e);
+					continue;
+				}
+				finally
+				{
+					IOUtil.close(stream);
+				}
+
+				FailSafeClassReader reader = new FailSafeClassReader(data, 0, data.length);
+
+				ClassWriter writer = new InstrumenterClassWriter(reader, isJdk6() ? ClassWriter.COMPUTE_FRAMES : ClassWriter.COMPUTE_MAXS, finder);
+
+				boolean modified = NotNullVerifyingInstrumenter.processClassFile(reader, writer);
+				if(modified)
+				{
+					changed = true;
+
+					FileOutputStream outputStream = null;
+					try
+					{
+						outputStream = new FileOutputStream(classFile);
+						IOUtil.copy(writer.toByteArray(), outputStream);
+
+						cacheLogic.putCacheEntry(classFile);
+					}
+					finally
+					{
+						IOUtil.close(outputStream);
+					}
+
+					//getLog().info("Processed: " + classFile.getPath());
+				}
+			}
+
+			if(!changed)
+			{
+				getLog().info("Nothing to instrument - all classes are up to date");
+			}
+			else
+			{
+				cacheLogic.write();
+			}
+		}
+		catch(Exception e)
+		{
+			getLog().error(e);
+		}
+	}
+
+	private InstrumentationClassFinder buildFinder() throws MalformedURLException, DependencyResolutionRequiredException
+	{
+		Collection<URL> classpath = new LinkedHashSet<URL>();
+		addParentClasspath(classpath, false);
+		addParentClasspath(classpath, true);
+
+		File javaHome = new File(System.getProperty("java.home"));
+
+		File rtJar = new File(javaHome, "lib/rt.jar");
+		if(rtJar.exists())
+		{
+			classpath.add(rtJar.toURI().toURL());
+		}
+
+		List<String> compileClasspathElements = myMavenProject.getCompileClasspathElements();
+		for(String compileClasspathElement : compileClasspathElements)
+		{
+			classpath.add(new File(compileClasspathElement).toURI().toURL());
+		}
+
+		return new InstrumentationClassFinder(classpath.toArray(new URL[classpath.size()]));
+	}
+
+	private boolean isJdk6()
+	{
+		// FIXME [VISTALL] we need this?
+		return true;
+	}
+
+	private boolean isJdk9()
+	{
+		return false;
+	}
+
+	protected File getClassFile(String className)
+	{
+		final String classOrInnerName = getClassOrInnerName(className);
+		if(classOrInnerName == null)
+		{
+			return null;
+		}
+		return new File(myMavenProject.getBuild().getOutputDirectory(), classOrInnerName + ".class");
+	}
+
+	protected String getClassOrInnerName(String className)
+	{
+		File classFile = new File(myMavenProject.getBuild().getOutputDirectory(), className + ".class");
+		if(classFile.exists())
+		{
+			return className;
+		}
+		int position = className.lastIndexOf('/');
+		if(position == -1)
+		{
+			return null;
+		}
+		return getClassOrInnerName(className.substring(0, position) + '$' + className.substring(position + 1));
+	}
+
+	private void addParentClasspath(Collection<URL> classpath, boolean ext) throws MalformedURLException
+	{
+		boolean isJava9 = isJdk9();
+		if(!isJava9)
+		{
+			String[] extDirs = System.getProperty("java.ext.dirs", "").split(File.pathSeparator);
+			if(ext && extDirs.length == 0)
+			{
+				return;
+			}
+
+			List<URLClassLoader> loaders = new ArrayList<URLClassLoader>(2);
+			for(ClassLoader loader = InstrumentMojo.class.getClassLoader(); loader != null; loader = loader.getParent())
+			{
+				if(loader instanceof URLClassLoader)
+				{
+					loaders.add(0, (URLClassLoader) loader);
+				}
+				else
+				{
+					getLog().warn("Unknown class loader: " + loader.getClass().getName());
+				}
+			}
+
+			for(URLClassLoader loader : loaders)
+			{
+				URL[] urls = loader.getURLs();
+				for(URL url : urls)
+				{
+					String path = urlToPath(url);
+
+					boolean isExt = false;
+					for(String extDir : extDirs)
+					{
+						if(path.startsWith(extDir) && path.length() > extDir.length() && path.charAt(extDir.length()) == File.separatorChar)
+						{
+							isExt = true;
+							break;
+						}
+					}
+
+					if(isExt == ext)
+					{
+						classpath.add(url);
+					}
+				}
+			}
+		}
+		else if(!ext)
+		{
+			parseClassPathString(ManagementFactory.getRuntimeMXBean().getClassPath(), classpath);
+		}
+	}
+
+	private void parseClassPathString(String pathString, Collection<URL> classpath)
+	{
+		if(pathString != null && !pathString.isEmpty())
+		{
+			try
+			{
+				StringTokenizer tokenizer = new StringTokenizer(pathString, File.pathSeparator + ',', false);
+				while(tokenizer.hasMoreTokens())
+				{
+					String pathItem = tokenizer.nextToken();
+					classpath.add(new File(pathItem).toURI().toURL());
+				}
+			}
+			catch(MalformedURLException e)
+			{
+				getLog().error(e);
+			}
+		}
+	}
+
+	private static String urlToPath(URL url) throws MalformedURLException
+	{
+		try
+		{
+			return new File(url.toURI().getSchemeSpecificPart()).getPath();
+		}
+		catch(URISyntaxException e)
+		{
+			throw new MalformedURLException(url.toString());
+		}
+	}
+}
---
+++ src/main/java/consulo/maven/notNullVerification/cache/CacheLogic.java
@@ -0,0 +1,128 @@
+package consulo.maven.notNullVerification.cache;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashSet;
+
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.IOUtil;
+
+/**
+ * @author VISTALL
+ * @since 29-May-17
+ */
+public class CacheLogic
+{
+	public static final String NAME = "notnull.cache";
+
+	private File myFile;
+
+	private HashSet<CacheEntry> myCacheEntries;
+
+	public CacheLogic(MavenProject mavenProject, String fileName)
+	{
+		myFile = new File(mavenProject.getBuild().getDirectory(), fileName);
+	}
+
+	public void read()
+	{
+		myCacheEntries = readImpl();
+	}
+
+	public void write()
+	{
+		if(myCacheEntries == null)
+		{
+			throw new IllegalArgumentException("#read() is not called");
+		}
+
+		HashSet<CacheEntry> cacheEntries = myCacheEntries;
+		myCacheEntries = null;
+
+		File parentFile = myFile.getParentFile();
+		parentFile.mkdirs();
+
+		ObjectOutputStream stream = null;
+		try
+		{
+			stream = new ObjectOutputStream(new FileOutputStream(myFile));
+			stream.writeObject(cacheEntries);
+		}
+		catch(Exception ignored)
+		{
+		}
+		finally
+		{
+			IOUtil.close(stream);
+		}
+	}
+
+	public boolean isUpToDate(File classFile)
+	{
+		CacheEntry cacheEntry = findCacheEntry(classFile);
+		return cacheEntry != null && cacheEntry.getClassTimestamp() == classFile.lastModified();
+	}
+
+	public CacheEntry findCacheEntry(File classFile)
+	{
+		if(myCacheEntries == null)
+		{
+			throw new IllegalArgumentException("#read() is not called");
+		}
+
+		for(CacheEntry cacheEntry : myCacheEntries)
+		{
+			if(cacheEntry.getClassFile().equals(classFile))
+			{
+				return cacheEntry;
+			}
+		}
+		return null;
+	}
+
+	private HashSet<CacheEntry> readImpl()
+	{
+		if(!myFile.exists())
+		{
+			return new HashSet<CacheEntry>();
+		}
+
+		ObjectInputStream stream = null;
+		try
+		{
+			stream = new ObjectInputStream(new FileInputStream(myFile));
+			return (HashSet<CacheEntry>) stream.readObject();
+		}
+		catch(Exception ignored)
+		{
+		}
+		finally
+		{
+			IOUtil.close(stream);
+		}
+		return new HashSet<CacheEntry>();
+	}
+
+	public boolean delete()
+	{
+		return myFile.exists() && myFile.delete();
+	}
+
+	public File getFile()
+	{
+		return myFile;
+	}
+
+	public void removeCacheEntry(File classFile)
+	{
+		myCacheEntries.remove(new CacheEntry(classFile, -1));
+	}
+
+	public void putCacheEntry(File clasFile)
+	{
+		myCacheEntries.add(new CacheEntry( clasFile, clasFile.lastModified()));
+	}
+}
